4852be0e1dc15ec800bbf8d38cb459f3940308f6
izainea <cizaineam@gmail.com>
2024-09-11 19:47:52 -0500

    [Ejercicio en la primera clase - Data WRANGLING con R]]

---
+++ Primer Trabajo.R
@@ -0,0 +1,32 @@
+## Instalar Libreria
+install.packages("readr")
+
+## Cargar Librería
+library(readr)
+
+## Cargar los datos
+titanic <- read_csv("Datos/titanic.csv")
+
+## Cargar  Excel
+#install.packages("readxl")
+#library(readxl)
+#datos_excel<-read_excel("ruta_al_archivo.xlsx")
+
+## Cargar Texto
+#datos_texto <- read.table("ruta_al_archivo.txt")
+
+## Cargar json
+#install.packages("jsonlite")
+#datos_json <- fromJSON("url.json")
+
+# Ver estructura del datframe
+
+str(titanic)
+
+## Ver resumen
+
+summary(titanic)
+
+# Seleccionar columnas específicas
+titanic_subset <- titanic[, c("Name", "Sex", "Age", "Survived")]
+head(titanic_subset)
